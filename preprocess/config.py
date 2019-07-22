7e5ab1616b364015d10b5d0126afd0c9ead3f0ae
Yao Shao <yshao998@gmail.com>
2019-07-22 19:50:19 +0800

    add config class

--- preprocess/config.py
+++ preprocess/config.py
@@ -1,1 +1,70 @@
-class Configure:
+####################################################################
+############## parameters
+# task
+#
+####### for label
+# label_vFn
+# label_vAnnFile
+#
+###### for crop
+# crop_vFn
+# crop_vAnnFile
+# outDirPos
+# outDirNeg
+# cropSize
+#
+###### example
+#
+# cfg = Configuration()
+# print(cfg.label_vFn, cfg.label_vAnnFile, cfg.crop_vFn, \
+#      cfg.crop_vAnnFile, cfg.outDirPos, cfg.outDirNeg, cfg.cropSize)
+#
+#####################################################################
+
+class Configuration:
+    def __init__(self):
+        self.__filePath = "../configure.txt"
+        fp_cfg = open(self.__filePath, 'r')
+        self.__strConfig = fp_cfg.readlines()
+
+        self.task = ''
+        self.label_vFn = ''
+        self.label_vAnnFile = ''
+        self.crop_vFn = ''
+        self.crop_vAnnFile = ''
+        self.outDirPos = ''
+        self.outDirNeg = ''
+        self.cropSize = []
+        self.initialize()
+
+    def initialize(self):
+        for line in self.__strConfig:
+            line = line.strip()
+            if line.find('gl_task') >= 0:
+                self.task = line.split('=')[1].strip()
+            if self.task == 'label':
+                if line.find('label_fn_video') >= 0:
+                    self.label_vFn = line.split('=')[1].strip()
+                if line.find('label_fn_annotation') >= 0:
+                    self.label_vAnnFile = line.split('=')[1].strip()
+            if self.task == 'crop':
+                if line.find('crop_fn_video') >= 0:
+                    self.crop_vFn = line.split('=')[1].strip()
+                if line.find('crop_fn_annotation') >= 0:
+                    self.crop_vAnnFile = line.split('=')[1].strip()
+                if line.find('crop_dir_pos') >= 0:
+                    self.outDirPos = line.split('=')[1].strip()
+                if line.find('crop_dir_neg') >= 0:
+                    self.outDirNeg = line.split('=')[1].strip()
+                if line.find('crop_size') >= 0:
+                    size = line.split('=')[1].strip()
+                    self.cropSize = [int(size.split(',')[0]), int(size.split(',')[1])]
+            if self.task == 'train' >= 0:
+                pass
+            if self.task == 'testROC' >= 0:
+                pass
+
+
+
+
+
