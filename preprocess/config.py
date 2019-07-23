7b5447bea5e159803f4edc46221f9211a5df9e77
Yao Shao <yshao998@gmail.com>
2019-07-23 11:02:01 +0800

    fix config

--- preprocess/config.py
+++ preprocess/config.py
@@ -37,18 +37,24 @@ class Configuration:
                 self.task = line.split('=')[1].strip()
             if self.task == 'label':
                 if line.find('label_fn_video') >= 0:
-                    self.label_vFn = line.split('=')[1].strip()
+                    tmp = line.split('=')[1].strip().split(',')
+                    self.label_vFn = [name.strip() for name in tmp]
                 if line.find('label_fn_annotation') >= 0:
-                    self.label_vAnnFile = line.split('=')[1].strip()
+                    tmp = line.split('=')[1].strip().split(',')
+                    self.label_vAnnFile = [name.strip() for name in tmp]
             if self.task == 'crop':
                 if line.find('crop_fn_video') >= 0:
-                    self.crop_vFn = line.split('=')[1].strip()
+                    tmp = line.split('=')[1].strip().split(',')
+                    self.crop_vFn = [name.strip() for name in tmp]
                 if line.find('crop_fn_annotation') >= 0:
-                    self.crop_vAnnFile = line.split('=')[1].strip()
+                    tmp = line.split('=')[1].strip().split(',')
+                    self.crop_vAnnFile = [name.strip() for name in tmp]
                 if line.find('crop_dir_pos') >= 0:
-                    self.outDirPos = line.split('=')[1].strip()
+                    tmp = line.split('=')[1].strip().split(',')
+                    self.outDirPos = [name.strip() for name in tmp]
                 if line.find('crop_dir_neg') >= 0:
-                    self.outDirNeg = line.split('=')[1].strip()
+                    tmp = line.split('=')[1].strip().split(',')
+                    self.outDirNeg = [name.strip() for name in tmp]
                 if line.find('crop_size') >= 0:
                     size = line.split('=')[1].strip()
                     self.cropSize = [int(size.split(',')[0]), int(size.split(',')[1])]
@@ -56,5 +62,3 @@ class Configuration:
                 pass
             if self.task == 'testROC' >= 0:
                 pass
-
-
